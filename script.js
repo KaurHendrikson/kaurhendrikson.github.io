c50e28323ef306fe2a7f0fbe7803a6420d98681f
Kaur Hendrikson <kaur@kaurh.com>
2024-08-18 22:28:21 +0300

    Update URL when a section is in view

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ window.addEventListener(
     },
     false
 );
+
+// Update the URL when a section is in view
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('div[id]');
+    const options = {
+        root: null, // Use the viewport as the root
+        threshold: 0.75, // Trigger when 75% of the section is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const id = entry.target.getAttribute('id');
+                history.replaceState(null, null, `#${id}`);
+            }
+        });
+    }, options);
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
